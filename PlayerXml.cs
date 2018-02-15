0a58d1f814a7aacc7f719339779adf1c7eeec4c1
OG <30807136+ntosguy@users.noreply.github.com>
2018-02-15 22:50:05 +0100

    Add files via upload

---
+++ PlayerXml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XmlSerialization
+{
+    [XmlRoot("PlayerConfiguration")]
+    public class PlayerXml
+    {
+        [XmlElement]
+        public string PlayerName { get; set; }
+
+        [XmlElement]
+        public int BaseHealth { get; set; }
+
+        [XmlElement]
+        public int AttackPoints { get; set; }
+
+        public void SaveXml()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(PlayerXml));
+
+            using (FileStream fs = new FileStream("players.xml", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                serializer.Serialize(fs, this);
+            }
+        }
+
+        public static PlayerXml LoadXml()
+        {
+            PlayerXml deserialized;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(PlayerXml));
+            using (FileStream stream = new FileStream("players.xml", FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                deserialized = (PlayerXml)serializer.Deserialize(stream);
+            }
+
+            return deserialized;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initial setup
+            PlayerXml player = new PlayerXml()
+            {
+                AttackPoints = 20,
+                BaseHealth = 100,
+                PlayerName = "Test Player 1"
+            };
+
+            // Serialize object
+            player.SaveXml();
+
+            // Deserialize object and print output
+            PlayerXml playerFromXml = PlayerXml.LoadXml();
+
+            Console.WriteLine(string.Format("Name: {0}\nHealth: {1}\nAttackPoints: {2}\n", playerFromXml.PlayerName, playerFromXml.BaseHealth, playerFromXml.AttackPoints));
+        }
+    }
+}
